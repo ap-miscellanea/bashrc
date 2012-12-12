0bc460d438202d5581b738b039929f6d41fa94b5
Aristotle Pagaltzis <pagaltzis@gmx.de>
2012-12-12 08:49:32 +0100

    namespace hostname aliases

--- rc.bash
+++ rc.bash
@@ -195,8 +195,8 @@ alias -- \
 	scp4='scp -c arcfour' \
 	singlecore='env HARNESS_OPTIONS= TEST_JOBS= MAKEFLAGS=' \
 
-case ${HOSTNAME%%.*} in heliopause|klangraum) alias f='ssh fernweh' ;; esac
-case ${HOSTNAME%%.*} in heliopause|fernweh) alias k='ssh klangraum' ;; esac
+case ${HOSTNAME%%.*} in heliopause|klangraum) alias @f='ssh fernweh' ;; esac
+case ${HOSTNAME%%.*} in heliopause|fernweh) alias @k='ssh klangraum' ;; esac
 
 if exists ionice ; then
 	case ${HOSTNAME%%.*} in
