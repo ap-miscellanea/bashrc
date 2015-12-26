3c29e50e974f318e106d5814689730ec7c1e846f
Aristotle Pagaltzis <pagaltzis@gmx.de>
2015-12-26 14:11:40 +0100

    reorder .profile more sensically

--- profile.bash
+++ profile.bash
@@ -1,7 +1,7 @@
-# for BSD-ish systems
-[ -x /usr/libexec/path_helper ] && eval `/usr/libexec/path_helper`
-
 [ -z "$SSH_AUTH_SOCK" -a -x /usr/bin/ssh-agent ] &&
 	LANG=C LC_ALL=C exec /usr/bin/ssh-agent /usr/bin/perl -e 'exec { shift } shift' "$SHELL" "$0"
+
+# for BSD-ish systems
+[ -x /usr/libexec/path_helper ] && eval `/usr/libexec/path_helper`
 
 [ -r ~/.bashrc ] && source ~/.bashrc
