e2ecacc37077faa60c01c953d8fae9dac2e64b20
Aristotle Pagaltzis <pagaltzis@gmx.de>
2017-06-09 16:07:41 +0200

    improve ssh-agent starter in profile

--- profile.bash
+++ profile.bash
@@ -1,5 +1,5 @@
 [ -z "$SSH_AUTH_SOCK" -a -x /usr/bin/ssh-agent ] &&
-	LANG=C LC_ALL=C exec /usr/bin/ssh-agent /usr/bin/perl -e 'exec { shift } shift' "$SHELL" "$0"
+	LANG=C LC_ALL=C exec /usr/bin/ssh-agent /usr/bin/perl -e 'exec { $ENV{SHELL} } @ARGV' -- "$0" ${1+"$@"}
 
 # for BSD-ish systems
 [ -x /usr/libexec/path_helper ] && eval `/usr/libexec/path_helper`
