a693b6b551f61286a0999064aba2f4f6d83c4ac7
Aristotle Pagaltzis <pagaltzis@gmx.de>
2026-09-25 11:00:51 +0200

    fix MANPATH setup

--- profile.bash
+++ profile.bash
@@ -1,7 +1,10 @@
 [ -z "$SSH_AUTH_SOCK" -a -x /usr/bin/ssh-agent ] &&
 	LANG=C LC_ALL=C exec /usr/bin/ssh-agent /usr/bin/perl -e 'exec { $ENV{SHELL} } @ARGV' -- "$0" ${1+"$@"}
 
-# for BSD-ish systems
-[ -x /usr/libexec/path_helper ] && eval `/usr/libexec/path_helper`
+# on BSD-ish systems, we want the default MANPATH to be made explicit, for later modifications
+[ -x /usr/libexec/path_helper ] &&
+	# The MANPATH environment variable will not be modified unless it is already set in the environment.
+	# -- path_helper(1)
+	eval `MANPATH=: /usr/libexec/path_helper`
 
 [ -r ~/.bashrc ] && source ~/.bashrc
